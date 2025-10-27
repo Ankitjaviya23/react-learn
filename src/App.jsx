8dbbf9f8fa04e16b48a58fa855687d59fb78d678
Ankit <ankitjaviya2@gmail.com>
2025-10-27 14:45:02 +0530

    1st day learn done

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <h1>Main Screen</h1>
+      <h3>Content Section</h3>
+      <div id="mainContain">Container section</div>
+    </>
+  );
+};
+
+export default App;
