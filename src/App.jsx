d19db9f0206bf4d6a498b0d6491e8303750db17e
Ankit <ankitjaviya2@gmail.com>
2025-10-28 11:32:51 +0530

    Day2 - Component setup & use

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import React from "react";
-
+import Header from "./component/Header";
+import Footer from "./component/Footer";
+import Card from "./component/Card";
 const App = () => {
   return (
     <>
+      <Header />
       <h1>Main Screen</h1>
       <h3>Content Section</h3>
       <div id="mainContain">Container section</div>
+
+      <Card />
+
+      <Footer />
     </>
   );
 };
---
+++ src/component/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <footer>
+        <p>
+          Author: Lapcare@2025
+          <br />
+        </p>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
